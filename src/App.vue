5344d4bac213b13d3cc0e9be139bd63d0d19b663
P0sseid0n <matheuspossidoniom@outlook.com>
2022-04-06 21:51:03 -0300

    Colocando a Open sans como a fonte family

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,9 @@
 <template></template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap");
+
+* {
+   font-family: "Open Sans", sans-serif;
+}
 </style>
