cfbbf91dd63e03808335df9c08cf9f90499aa967
P0sseid0n <matheuspossidoniom@outlook.com>
2022-04-09 11:26:18 -0300

    Font awesome instalado

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add(fas, far, fab)
+
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 
