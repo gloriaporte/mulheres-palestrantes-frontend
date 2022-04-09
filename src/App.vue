9c547611e1ad1d06593cb7133651f3462155c749
P0sseid0n <matheuspossidoniom@outlook.com>
2022-04-09 18:06:51 -0300

    Informação principal adicionada

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,16 @@ const store = useWomanStore()
 </script>
 
 <template>
+   <section>
+      <h2>Procurando mulheres para palestrar em seu evento?</h2>
+      <h3>O Mulheres Palestrantes é uma lista de mulheres que falam sobre assuntos relacionados a programação, design, empreendedorismo e tecnologia num geral.</h3>
+   </section>
    <main>
       <WomanCard v-for="woman in store.all" :woman="woman" />
    </main>
 </template>
 
-<style>
+<style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap");
 
 * {
@@ -25,12 +29,29 @@ const store = useWomanStore()
 
 #app {
    background-color: #f8f8f8;
-}
 
-#app > main {
-   display: flex;
-   flex-direction: row;
-   flex-wrap: wrap;
-   gap: 32px;
+   > section {
+      text-align: center;
+      padding: 48px 32px;
+
+      h2 {
+         font-size: 32px;
+         color: #333;
+         font-weight: 400;
+      }
+
+      h3 {
+         font-size: 24px;
+         color: #444;
+         font-weight: 700;
+      }
+   }
+
+   > main {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      gap: 32px;
+   }
 }
 </style>
